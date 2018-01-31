093adcb5a19da9adda5a32c9428ed58ea0e47325
Ervin Mamutov <erviin@live.ie>
2018-01-31 18:20:34 +0000

    Managed to get the recursive function working trying to figure out what cons does

--- [2]CollatzList/collatzlist.rkt
+++ [2]CollatzList/collatzlist.rkt
@@ -1,8 +1,23 @@
 #lang racket
-(list 1 2 3 4) ; create a list
-(define l '( 1 2 3 4)) ; define l is a a list
-l
-(cons 1 (cons 2 (cons 3 (cons 4 (cons 5 null))))) ; construct a list from cons of a and b
-(append '(1 2) '(3 4)) ; Add to lists together using append
-(append l 1) ; Try to append 1 to the end of l produces '(1 2 3 4 . 1)
-(append l (cons 1 null)) ; Successfully got rid of the . using a cons of 1 and null
+(define listOfNs '())
+
+(define (collatzlist n)
+  (append listOfNs n)
+  (if (<= n 1)
+      listOfNs
+      (if (odd? n)
+          (collatzlist (odd n))
+          (collatzlist (even n)))))
+          
+
+(define (odd n)
+  (+ (* 3 n) 1)
+ )
+
+(define (even n)
+  (/ n 2)
+ )
+
+(odd 4)
+(even 4)
+(collatzlist 10)
